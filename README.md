76257922e33d32d285949443d049499e90aa9dbc
ocsarpo <ocsarpo22@gmail.com>
2018-08-23 02:02:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+// http://niceman.tistory.com/91            AES 암호화
+// http://newsight.tistory.com/50           파일 읽고 쓰기(디렉 생성 후 파일 생성)
+// http://oneday0012.tistory.com/112        에뮬레이터에서 파일 위치보기
+// http://codetravel.tistory.com/25         파일 읽고 쓰기
+```
+
---
+++ app/src/main/java/com/example/postw/myapplication/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.postw.myapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et;
+    private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.editText);
+        tv = findViewById(R.id.target);
+    }
+
+    public void backup(View view){
+        setPermission();
+        String t = et.getText().toString();
+
+        try {
+            AES256Chiper.secretKey = "1234567890123456";
+            String enc = AES256Chiper.AES_Encode(t);
+
+            if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+                File dir= new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "MyDirec");
+                if (!dir.exists()){
+                    dir.mkdirs();
+                }
+                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "/MyDirec/Data.enc");
+                file.createNewFile();
+
+                FileWriter fw = new FileWriter(file, false);
+                fw.write(enc);
+                fw.close();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void recovery(View view){
+        try{
+            String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/MyDirec/Data.enc";
+            StringBuffer buffer = new StringBuffer();
+            BufferedReader eReader = new BufferedReader(new FileReader(path));
+            String data = eReader.readLine();
+            while(data != null){
+                buffer.append(data);
+                data = eReader.readLine();
+            }
+            AES256Chiper.secretKey = "1234567890123456";
+            String dec = AES256Chiper.AES_Decode(buffer.toString());
+            tv.setText(dec);
+            eReader.close();
+        }catch (Exception  e){
+            e.printStackTrace();
+        }
+
+    }
+
+    // 권한 확인 후 대화상자 보여줌
+    private void setPermission() {
+        int permissionInfo = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(permissionInfo == PackageManager.PERMISSION_GRANTED)
+            Toast.makeText(getApplicationContext(), "SDCard 쓰기 권한 있음", Toast.LENGTH_SHORT).show();
+        else{
+            // 재요청
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                Toast.makeText(getApplicationContext(), "권한의 필요성 설명", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
+    }
+
+    // 해당 권한에 따른 사용자 수락여부 확인
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        String str = null;
+
+        if(requestCode == 100){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                str = "SDCard 쓰기권한 승인";
+            else
+                str = "SDCard 쓰기권한 거부";
+            Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
